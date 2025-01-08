f891fac4356939c04760fd4f6a2b34bab1938945
setloth <setheweintraub@me.com>
2025-01-08 15:01:05 -0500

    1.9.1 - Slight config/internal update

--- src/main/java/me/setloth/modificationMaster/ModificationMaster.java
+++ src/main/java/me/setloth/modificationMaster/ModificationMaster.java
@@ -9,7 +9,6 @@ import me.setloth.modificationMaster.util.VersionChecker;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 import java.util.logging.Level;
